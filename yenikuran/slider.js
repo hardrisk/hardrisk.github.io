a9492b67977a0ab782ff2be91d5a18f7c1fea81f
hardrisk <hardrisk@users.noreply.github.com>
2019-02-19 22:37:38 +0300

    Add files via upload

---
+++ yenikuran/slider.js
@@ -0,0 +1,39 @@
+﻿$(function(){
+
+	var slider = $('.slider'),
+		list = slider.find('ul.slider_liste'),
+		length = list.find('li').length,
+		width = slider.outerWidth(),
+		totalWidth = width * length,
+		index = 0,
+		next = $('a.sonraki', slider),
+		prev = $('a.onceki', slider);
+		
+	list.find('li').width(width).end().width(totalWidth);
+	
+	/* responsive */
+	$(window).resize(function(){
+		width = slider.outerWidth();
+		totalWidth = width * length;
+		list.find('li').width(width).end().width(totalWidth).css('margin-left', '-' + (index * width) + 'px');
+	});
+	
+	/* sonraki */
+	next.on('click', function(){
+		if ( index < length - 1 ) index++;
+		list.stop().animate({
+			marginLeft: '-' + (index * width)
+		}, 50);
+		return false
+	});
+	
+	/* önceki */
+	prev.on('click', function(){
+		if ( index > 0 ) index--;
+		list.stop().animate({
+			marginLeft: '-' + (index * width)
+		}, 50);
+		return false
+	});
+
+});
